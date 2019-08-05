12d9267f8f69b13607b77107539324e4a2c721e3
MGraf <info@moritz-graf.de>
2019-08-05 14:39:51 +0200

    function_exists check

--- web/app/themes/template/functions/acf--optionspage.php
+++ web/app/themes/template/functions/acf--optionspage.php
@@ -1,8 +1,12 @@
 <?php
 
-acf_set_options_page_menu('optionen');
+if( function_exists('acf_set_options_page_menu') && function_exists('acf_add_options_sub_page') ):
+
+	acf_set_options_page_menu('optionen');
 	acf_add_options_sub_page(array(
 	    'page_title' => __('Optionen'),
 	    'menu_title' => __('Optionen'),
 	    'menu_slug' => 'optionen',
 	));
+
+endif;
